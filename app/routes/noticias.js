3e0d5162b140c8c9f55c1af2723db94de5b1a29e
unknown <Suporte@STFCPGC033950-W.stefanini.dom>
2019-07-08 13:49:12 -0300

    Criando conexão com banco de dados mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,19 @@
 module.exports = function(app) {
+
     app.get('/noticias', function(req, res) {
-        res.render("noticias/noticias")
+
+        const mysql = require('mysql');
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'teste',
+            password: 'BancoTeste12!@',
+            database: 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function(error, result) {
+            res.send(result);
+            console.log(error);
+        });
+        //res.render("noticias/noticias")
     });
-}
+};
