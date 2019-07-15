8a0c6828b891eae3233c248437da8279efacd263
David Luna <dbluna@STFCPGC033950-W>
2019-07-15 11:15:06 -0300

    Refatorando rotas do projeto com o consign

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
 const app = require('./config/server')
 
-const rotaNoticias = require('./app/routes/noticias')(app);
-
-const rotaHome = require('./app/routes/home')(app);
-
-const rotaFormulario = require('./app/routes/formularioInclusaoNoticia')(app);
-
 app.listen(3000, function() {
     console.log("Server ON");
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const app = express();
+const consign = require('consign');
 
+const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views')
+
+consign().include('app/routes').into(app);
 
 module.exports = app;
