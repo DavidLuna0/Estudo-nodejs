0f52cd3c522f236efd506391b3becc8aab761811
David Luna <dbluna@STFCPGC033950-W>
2019-07-15 15:28:06 -0300

    Exibindo erros de validação e mantendo preenchimento do formulario

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,15 +1,29 @@
 module.exports = function(application) {
     application.get('/formulario_inclusao_noticia', function(req, res) {
-        res.render("admin/form_add_noticia")
+        res.render("admin/form_add_noticia", {validacao: {}, noticia : {}})
     });
 
     application.post('/noticias/salvar', function(req, res) {
-        const noticias = req.body;
+        const noticia = req.body;
+
+        req.assert('titulo', 'Titulo é obrigatorio').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatorio').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor é obrigatorio').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatorio').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia', 'Noticia é obrigatorio').notEmpty();
+
+        const erros = req.validationErrors();
+
+        if(erros) {
+            res.render("admin/form_add_noticia", {validacao : erros, noticia : noticia});
+            return;
+        }
 
         const connection = application.config.dbconnection();
         const noticiasModel = new application.app.models.NoticiasDAO(connection);
 
-        noticiasModel.salvarNoticia(noticias, function(error, result){
+        noticiasModel.salvarNoticia(noticia, function(error, result){
             res.redirect("/noticias");
         })
     });
