657e4f08241abe27b79aec22fb4dbdec7a1c486a
David Luna <dbluna@STFCPGC033950-W>
2019-07-16 09:43:24 -0300

    Reestruturando diretorio de estudos

--- App.js
+++
@@ -1,6 +0,0 @@
-const app = require('./config/server')
-
-app.listen(3000, function() {
-    console.log("Server ON");
-});
-
--- mod_teste.js
+++
@@ -1,4 +0,0 @@
-module.exports = function() {
-    var msg = "Esse modulo contem apenas uma string";
-    return msg;
-}
