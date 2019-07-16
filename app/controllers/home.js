7e064e9f40aca2e64f466a8047816544e1a9c04b
David Luna <dbluna@STFCPGC033950-W>
2019-07-16 07:35:00 -0300

    Refatorando home e incluindo recursos estaticos

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,8 @@
 module.exports.index = function(application, req, res) {
-    res.render("home/index");
+    const connection = application.config.dbconnection();
+    const noticiasModel = new application.app.models.NoticiasDAO(connection);
+    noticiasModel.getUltimasNoticias(function(error, result) {
+        res.render("home/index", {noticias : result});
+    });
+    
 }
