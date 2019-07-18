bf1f361d3d4bb96a038aff464d048d1c0d6486da
David Luna <dbluna@STFCPGC033950-W>
2019-07-18 15:24:56 -0300

    Criando controller de autenticação

--- MmoGot/app/controllers/index.js
+++ MmoGot/app/controllers/index.js
@@ -1,3 +1,19 @@
 module.exports.home = function(application, req, res) {
-    res.render('index');
+    res.render('index', {validacao: {}});
+}
+
+module.exports.autenticar = function(application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'Usuario nao pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha nao deve ser vazia').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros) {
+        res.render("index", {validacao: erros});
+        return;
+    }
+    
+    res.send('Tudo ok');
 }
