0d99a5a082ad6fe080a6417bba054e0ac3327da6
David Luna <dbluna@STFCPGC033950-W>
2019-07-18 14:20:25 -0300

    Configurando banco mongoDb

---
+++ MmoGot/config/dbConnection.js
@@ -0,0 +1,19 @@
+const mongo = require('mongodb');
+
+const connMongoDb = function() {
+    var db = new mongo.Db(
+        'got',
+        new mongo.Server(
+            'localhost',
+            27017,
+            {}
+        ),
+        {}
+    );
+
+    return db;
+}
+
+module.exports = function() {
+    return connMongoDb;
+}
