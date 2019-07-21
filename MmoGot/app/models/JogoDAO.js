efc5dfc3433a48b04daa9cdb05a7e28bae266ac8
DavidLuna0 <ddavidr569@gmail.com>
2019-07-21 19:30:55 -0300

    Exibindo as ações na aba de pergaminhos

--- MmoGot/app/models/JogoDAO.js
+++ MmoGot/app/models/JogoDAO.js
@@ -37,11 +37,11 @@ JogoDAO.prototype.acao = function(acao) {
             var date = new Date();
 
             var tempo = null;
-            switch(acao.acao) {
-                case 1: tempo = 1 * 60 * 60000;
-                case 2: tempo = 2 * 60 * 60000;
-                case 3: tempo = 5 * 60 * 60000;
-                case 4: tempo = 5 * 60 * 60000;
+            switch(parseInt(acao.acao)) {
+                case 1: tempo = 1 * 60 * 60000; break;
+                case 2: tempo = 2 * 60 * 60000; break;
+                case 3: tempo = 5 * 60 * 60000; break;
+                case 4: tempo = 5 * 60 * 60000; break;
             }
             acao.acao_termina_em = date.getTime() + tempo;
             collection.insert(acao);
@@ -50,11 +50,12 @@ JogoDAO.prototype.acao = function(acao) {
     });
 }
 
-JogoDAO.prototype.getAcoes = function(usuario) {
+JogoDAO.prototype.getAcoes = function(usuario, res) {
     this._connection.open(function (err, mongoclient) {
         mongoclient.collection("acao", function(err, collection) {
             collection.find({usuario : usuario}).toArray((err, result) => {
-                console.log(result);
+                res.render("pergaminhos",  {acoes: result});
+                mongoclient.close();
             })
         })
     });
