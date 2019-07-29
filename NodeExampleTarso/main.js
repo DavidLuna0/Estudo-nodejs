3e51bb07ce4942ac437b680b1b96fa67d34e30fa
DavidLuna0 <ddavidr569@gmail.com>
2019-07-29 11:25:44 -0300

    Usando modulos personalizados e entendendo ordem de carregamento do node

--- NodeExampleTarso/main.js
+++ NodeExampleTarso/main.js
@@ -1,16 +1,11 @@
+const fatorial = require('./fatorial')/* .fatorial */
 console.log('n-fatorial');
 
 /* console.log(`Executando o script a partir do diretorio ${process.cwd()}`)
 process.on('exit', () => {
     console.log("Script esta prestes a terminar");
 }) */
-const fatorial = (num) => {
-    if(num === 0) {
-        return 1;
-    }
 
-    return num * fatorial(num - 1);
-}
 
 const num = parseInt(process.argv[2]);
 
