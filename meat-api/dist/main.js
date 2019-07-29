00bdab15a8e03666126ef7d8911be9dbfb49f5ef
DavidLuna0 <ddavidr569@gmail.com>
2019-07-29 14:24:39 -0300

    Criando get para testar restify

--- meat-api/dist/main.js
+++ meat-api/dist/main.js
@@ -1,1 +1,14 @@
-console.log('main script 2...');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0'
+});
+server.get('/home', (req, res, next) => {
+    res.json({ message: 'hello' });
+    return next();
+});
+server.listen(3000, () => {
+    console.log("Servidor iniciado na porta 3000");
+});
--- meat-api/main.ts
+++ meat-api/main.ts
@@ -1,1 +1,15 @@
-console.log('main script 2...')
+import * as restify from 'restify';
+
+const server = restify.createServer({
+    name:'meat-api',
+    version: '1.0.0'
+});
+
+server.get('/home', (req, res, next) => {
+    res.json({message: 'hello'})
+    return next();
+})
+
+server.listen(3000, () => {
+    console.log("Servidor iniciado na porta 3000");
+})
