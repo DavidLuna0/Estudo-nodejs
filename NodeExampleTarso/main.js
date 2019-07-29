a833d22ad50237f9e096018143a307f0ee938f00
DavidLuna0 <ddavidr569@gmail.com>
2019-07-29 11:42:27 -0300

    Entendendo funcionamento do npm

--- NodeExampleTarso/main.js
+++ NodeExampleTarso/main.js
@@ -1,14 +1,10 @@
-const fatorial = require('./fatorial')/* .fatorial */
+const fatorial = require('./fatorial')
 console.log('n-fatorial');
+const argv = require('yargs').demandOption('num').argv;
 
-/* console.log(`Executando o script a partir do diretorio ${process.cwd()}`)
-process.on('exit', () => {
-    console.log("Script esta prestes a terminar");
-}) */
-
-
-const num = parseInt(process.argv[2]);
-
+const num = argv.num;
 //Atraves do objeto process é criado uma ponte entre o script executando e a maquina onde está executado
 
 console.log(`O fatorial de ${num} é igual a ${fatorial(num)}`);
+
+console.log(module.paths);
